f0b20bfe533b2ab5fc331f4c4e81b9e910175fbe
Walaa Mostafa <www39z2010@gmail.com>
2026-09-13 16:37:49 +0300

    console Pyramid Maker.py

--- console Pyramid Maker.py
+++ console Pyramid Maker.py
@@ -22,9 +22,6 @@ elif  pyramid_type == "isosceles" :
             print("*", end="")
 
         print()
-
-    # for i in range (1,n+1) :
-    #     print(" " * (n - i) + "*" * (2 * i - 1))
         
 else :
     print("Exit")
