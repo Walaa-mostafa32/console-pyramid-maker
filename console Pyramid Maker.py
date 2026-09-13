faf58a87b32b8279f9ae188cc418e0b676dc1683
Walaa Mostafa <www39z2010@gmail.com>
2026-09-13 16:29:21 +0300

    Add files via upload

---
+++ console Pyramid Maker.py
@@ -0,0 +1,30 @@
+# console Pyramid Maker 
+
+pyramid_type = input("Enter the pyramid type:")
+
+n = int(input("Enter the number of layers:" ))
+
+if pyramid_type == "right" :
+
+    for i in range (n) :
+        print( )
+        
+        for j in range (i+1) :
+            print("* ", end="")
+
+elif  pyramid_type == "isosceles" :
+    for i in range(n):
+
+        for j in range(n - i - 1):
+            print(" ", end="")
+
+        for j in range(2 * i + 1):
+            print("*", end="")
+
+        print()
+
+    # for i in range (1,n+1) :
+    #     print(" " * (n - i) + "*" * (2 * i - 1))
+        
+else :
+    print("Exit")
